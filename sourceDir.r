45d0a7d95c83a2e61be14fa434ac29320c4bcb93
dfortin <dfortin@iastate.edu>
2012-07-20 12:40:53 -0500

    added documentation to sourceDir

--- sourceDir.r
+++ sourceDir.r
@@ -1,5 +1,10 @@
-
-
+#' Sources all R files in the directory
+#'
+#' Sources all R and S files in the directory specified by the path variable
+#'
+#' @param path path of the directory
+#' @param trace logical If equal to TRUE sourced files will be printed to screen
+#' @param ... arguments passed to source()
 sourceDir <-  function(path, trace = TRUE, ...) {
     for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
        if(trace) cat(nm,":")           
